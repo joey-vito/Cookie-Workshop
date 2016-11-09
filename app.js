bb362cab2dcfbdf59343c1b61a4c6fae634e2215
Joey Johnson <joey.johnson7@icloud.com>
2016-11-09 12:15:38 -0500

    Completed project

--- app.js
+++ app.js
@@ -1,64 +1,47 @@
 $(document).ready(function() {
     console.log("ready!");
     firstTime();
-    // checkCookie();
 
 });
 
 
-var chocoVal= $("#choco").val();
-var sugarVal= $("#sugar").val();
-var lemonVal= $("#lemon").val();
+var chocoVal;
+var sugarVal;
+var lemonVal;
 
 function firstTime() {
-	var value = Cookies.get("choco", "sugar", "lemon");
-	if(value !== ""){
-		$("#sidebar").show();
-		}else{
+	if(document.cookie === ""){
 		$("#sidebar").hide();
+		}else{
+		$("#sidebar").show();
+		orderHistory();
 	}
 }
+
 function setCookie() {
+	chocoVal= $("#choco").val();
+	sugarVal= $("#sugar").val();
+	lemonVal= $("#lemon").val();
 	Cookies.set("choco", chocoVal);
 	Cookies.set("sugar", sugarVal);
 	Cookies.set("lemon", lemonVal);
-	console.log("document.cookie")
+	console.log(document.cookie);
 }
 
 function orderHistory() {
-	$("#choco_val").html(chocoVal);
-	$("#sugar_val").html(sugarVal);
-	$("#lemon_val").html(lemonVal);
+	$("#choco_val").html(Cookies.get("choco"));
+	$("#sugar_val").html(Cookies.get("sugar"));
+	$("#lemon_val").html(Cookies.get("lemon"));
 }
 
 function expireCookies() {
-	Cookies.expire("chocoVal");
-	Cookies.expire("sugarVal");
-	Cookies.expire("lemonVal");
+	Cookies.expire("choco");
+	Cookies.expire("sugar");
+	Cookies.expire("lemon");
 	console.log("They gone!");
 }
 
 
-
-
-// function checkCookie() {
-// 	var value = Cookies.get("name");
-// 	if (value !== "") {
-// 		// $('#header').innerHTML = "Welcome " + value;
-// 	} else {
-// 		console.log("")
-// 	}
-
-// }
-
-
-// function setAnimation() {
-// 	if(Cookies.get("visited")){
-// 		$("#sidebar").show();
-// 	} else {
-// 		Cookies.set("visited", true);
-// 	}
-// }
 
 
 
